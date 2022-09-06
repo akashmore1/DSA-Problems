04beed1d67a0660cbb9c44b4b14c6cd0bf1b95e2
Akash More <akashkmore1@gmail.com>
2022-09-06 22:58:19 +0530

    Write push method for singly linked list

--- PushPopLinkedList.js
+++ PushPopLinkedList.js
@@ -11,4 +11,21 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+      this.length++;
+      return this;
+    }
+
+    this.tail.next = newNode;
+    this.tail = this.tail.next;
+    this.length++;
+
+    return this;
+  }
 }
