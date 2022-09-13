762b89f14c56cecaed8ba99d46376cdada971724
Akash More <akashkmore1@gmail.com>
2022-09-13 21:31:01 +0530

    Singly Linked list initial setup

---
+++ SLL_Insert_Remove_Reverse.js
@@ -0,0 +1,118 @@
+"use strict";
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+      this.length++;
+      return this;
+    }
+
+    this.tail.next = newNode;
+    this.tail = this.tail.next;
+    this.length++;
+
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+      return this;
+    }
+
+    return current;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+      this.length++;
+      return this;
+    }
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let current = this.head;
+    this.head = current.next;
+    current.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return undefined;
+    }
+
+    let current = this.head;
+
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+
+    return current;
+  }
+
+  set(index, value) {
+    let node = this.get(index);
+    node.val = value;
+  }
+}
+
+let sl = new SinglyLinkedList();
+sl.push(1);
+sl.push(2);
+sl.push(3);
+sl.push(4);
+sl.push(5);
+sl.push(6);
