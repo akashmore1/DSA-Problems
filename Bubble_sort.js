94975f4fe26e01a5350d30c9bfa1784574a6d33e
Akash More <akashkmore1@gmail.com>
2022-09-15 18:15:47 +0530

    Bubble sort code

--- Bubble_sort.js
+++ Bubble_sort.js
@@ -7,17 +7,17 @@
 3. repeat this process until whole array is sorted.
 */
 
-// const bubbleSort = (nums) => {
-//   for (let i = nums.length - 1; i >= 0; i--) {
-//     for (let j = 0; j < nums.length; j++) {
-//       if (nums[j] > nums[j + 1]) {
-//         let temp = nums[j];
-//         nums[j] = nums[j + 1];
-//         nums[j + 1] = temp;
-//       }
-//     }
-//   }
-//   return nums;
-// };
+const bubbleSort = (nums) => {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[j] > nums[j + 1]) {
+        let temp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
+      }
+    }
+  }
+  return nums;
+};
 
-// console.log(bubbleSort([3, 2, 1, 4, 9, -3, 2, 0]));
+console.log(bubbleSort([3, 2, 1, 4, 9, -3, 2, 0]));
