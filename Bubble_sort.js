b8c02a37e5e9bd558d56a71a3dc370cd3f0f2464
Akash More <akashkmore1@gmail.com>
2022-09-15 18:19:38 +0530

    Remove commented code

--- Bubble_sort.js
+++ Bubble_sort.js
@@ -15,9 +15,6 @@ const bubbleSort = (nums) => {
   for (let i = nums.length - 1; i >= 0; i--) {
     for (let j = 0; j < nums.length; j++) {
       if (nums[j] > nums[j + 1]) {
-        // let temp = nums[j];
-        // nums[j] = nums[j + 1];
-        // nums[j + 1] = temp;
         swap(nums, j, j + 1);
       }
     }
